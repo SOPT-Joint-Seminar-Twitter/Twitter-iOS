9574963dd4068d755dd735048b70d5d46f6269e0
suyeon <rlatndus9911@naver.com>
2022-05-15 19:45:46 +0900

    [FEAT] 프로젝트 생성

---
+++ Twitter-iOS/Twitter-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter-iOS
+//
+//  Created by 김수연 on 2022/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
