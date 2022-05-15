e5a8609e1c11fdd774a2da5a6fb1ffa1a121bed1
suyeon <rlatndus9911@naver.com>
2022-05-15 20:20:05 +0900

    [MOVE] 프로젝트 폴더링

---
+++ Twitter-iOS/Twitter-iOS/Screens/Writing/Suyeon/ViewController/SuYeonWritingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SuYeonWritingViewController.swift
+//  Twitter-iOS
+//
+//  Created by 김수연 on 2022/05/15.
+//
+
+import UIKit
+
+class SuYeonWritingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
