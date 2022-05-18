67335a551add5cc9e7e0985f74cd288eb670baaf
MINJAE KIM <ffalswo2@naver.com>
2022-05-18 22:10:57 +0900

    [Add] 공유하기 아이콘 에셋 추가 (#10)

--- Twitter-iOS/Twitter-iOS/Global/Literal/ImageLiteral.swift
+++ Twitter-iOS/Twitter-iOS/Global/Literal/ImageLiteral.swift
@@ -24,6 +24,7 @@ enum ImageLiteral {
         static var iconPlus: UIImage { .load(named: "iconPlus")}
         static var iconSound: UIImage { .load(named: "iconSound")}
         static var iconMoreBtn: UIImage { .load(named: "iconMoreButton")}
+        static var iconShareBtn: UIImage { .load(named: "iconMShareButton")}
 
         static var imgProfile1: UIImage { .load(named: "imgProfile1")}
         static var imgProfile2: UIImage { .load(named: "imgProfile2")}
