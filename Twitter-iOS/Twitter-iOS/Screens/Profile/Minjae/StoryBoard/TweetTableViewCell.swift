2726d0da34c80c039a602787fe93774d36018a2d
MINJAE KIM <ffalswo2@naver.com>
2022-05-18 19:18:48 +0900

    [Add] TweetTableViewCell 생성 (#10)

---
+++ Twitter-iOS/Twitter-iOS/Screens/Profile/Minjae/StoryBoard/TweetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter-iOS
+//
+//  Created by 김민재 on 2022/05/18.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
