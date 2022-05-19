cdb63d7a3742b38127d3fb113729d222998803e8
MINJAE KIM <ffalswo2@naver.com>
2022-05-20 02:54:55 +0900

    [Feat] 트윗 테이블 뷰 UI 구현 (#10)

--- Twitter-iOS/Twitter-iOS/Screens/Profile/Minjae/ViewController/TweetViewController.swift
+++ Twitter-iOS/Twitter-iOS/Screens/Profile/Minjae/ViewController/TweetViewController.swift
@@ -9,9 +9,45 @@ import UIKit
 
 class TweetViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var tweetDataList = TweetResponse.sampleData
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        registerXib()
+        setTableView()
     }
+
+    private func registerXib() {
+        let nib = UINib(nibName: TweetTableViewCell.identifier, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: TweetTableViewCell.identifier)
+    }
+
+    private func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 500
+    }
+}
+
+extension TweetViewController: UITableViewDelegate {
+
+}
+
+extension TweetViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TweetResponse.sampleData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as? TweetTableViewCell else { return UITableViewCell() }
+        
+        cell.setData(dataModel: tweetDataList[indexPath.row])
+        return cell
+    }
+
 }
