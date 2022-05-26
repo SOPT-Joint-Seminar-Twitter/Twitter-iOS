53d1363335440b9dd48266f4604de151447bba2e
suyeon <rlatndus9911@naver.com>
2022-05-27 01:38:46 +0900

    [FEAT] 서버 파일 세팅

---
+++ Twitter-iOS/Twitter-iOS/NetworkResult.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkResult.swift
+//  Twitter-iOS
+//
+//  Created by 김수연 on 2022/05/21.
+//
+
+import Foundation
+
+/*
+ NetworkResult : 네트워크 결과 나누기
+ */
+
+enum NetworkResult<T> {
+    case success(T)
+    case requestErr(T)
+    case pathErr
+    case serverErr
+    case networkFail
+}
