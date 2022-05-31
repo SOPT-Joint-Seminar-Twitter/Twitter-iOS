18dea027021585608c5e79643c15b0e2ff990f12
Kim Minjae <ffalswo2@naver.com>
2022-05-31 19:53:09 +0900

    Feat: 유저 정보 조회 API 구현

--- Twitter-iOS/Twitter-iOS/Screens/Profile/Minjae/ViewController/ProfileViewController.swift
+++ Twitter-iOS/Twitter-iOS/Screens/Profile/Minjae/ViewController/ProfileViewController.swift
@@ -8,18 +8,27 @@
 import UIKit
 
 class ProfileViewController: UIViewController, UITableViewDelegate {
-
+    
+    // MARK: UI
     @IBOutlet weak var backArrow: UIButton!
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var profileIdLabel: UILabel!
+    @IBOutlet weak var introductionLabel: UILabel!
+    @IBOutlet weak var createdAtLabel: UILabel!
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var tabStackView: UIStackView!
     @IBOutlet weak var barView: UIView!
     @IBOutlet weak var barBackgroundView: UIView!
+    
+    // MARK: Passing Data
+    private var userKeyId = ""
+    
+    // MARK: Constraints
     @IBOutlet weak var barViewLeading: NSLayoutConstraint!
     @IBOutlet weak var barViewWidth: NSLayoutConstraint!
-    
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var containerViewHeight: NSLayoutConstraint!
     
@@ -38,8 +47,8 @@ class ProfileViewController: UIViewController, UITableViewDelegate {
     private var currentIdx = 0
     private var btnTapped = false
     
+    // MARK: Floating Button
     private let floatingButton = UIButton()
-    
     private func setFloatingButton() {
         floatingButton.frame = CGRect(x: self.view.frame.size.width - 112, y: UIScreen.main.bounds.height - 160, width: 56, height: 56)
         floatingButton.setImage(ImageLiteral.Writing.iconBigPlus, for: .normal)
@@ -53,15 +62,22 @@ class ProfileViewController: UIViewController, UITableViewDelegate {
         self.present(writingVC, animated: true)
     }
     
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setProfileUI()
         setFloatingButton()
         setPageVC()
-        
+        getUserInfo()
         scrollView.delegate = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    // MARK: Custom Function
     private func setProfileUI() {
         makeImageCircle(profileImageView)
         makeButtonCircle(backArrow)
@@ -245,5 +261,38 @@ extension UIPageViewController {
     var scrollView: UIScrollView? {
 
         return view.subviews.filter { $0 is UIScrollView }.first as? UIScrollView
+    }
+}
+
+
+extension ProfileViewController {
+    private func getUserInfo() {
+
+        UserService.shared.getUserInfo { result in
+            switch result {
+            case .success(let data):
+                guard let data = data as? UserResponse else { return }
+                self.userKeyId = data.id
+                self.profileNameLabel.text = data.userName
+                self.profileIdLabel.text = data.userId
+                self.introductionLabel.text = data.introduce
+                
+                if let date = data.createdAt.toDate() {
+                    let calendarDate = Calendar.current.dateComponents([.year, .month], from: date)
+                    if let year = calendarDate.year, let month = calendarDate.month {
+                        self.createdAtLabel.text = "\(year)년 \(month)월에 가입함"
+                    }
+                }
+                
+            case .requestErr:
+                print("requestErr")
+            case .pathErr:
+                print("pathErr")
+            case .serverErr:
+                print("serverErr")
+            case .networkFail:
+                print("networkFail")
+            }
+        }
     }
 }
