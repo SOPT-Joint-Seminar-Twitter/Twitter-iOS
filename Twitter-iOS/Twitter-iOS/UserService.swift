17fad8fcab4f5446aaa95ae8ed5c6ee85f5cecd5
Kim Minjae <ffalswo2@naver.com>
2022-05-31 19:51:46 +0900

    Feat: UserService 파일 생성
    - 유저 정보, 트윗 불러오기, 좋아요 누르기 관련 service 구현

---
+++ Twitter-iOS/Twitter-iOS/UserService.swift
@@ -0,0 +1,70 @@
+//
+//  UserService.swift
+//  Twitter-iOS
+//
+//  Created by 김민재 on 2022/05/31.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+class UserService: BaseService {
+    static let shared = UserService()
+
+    private override init() { }
+
+    func getUserInfo(completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        AFmanager.request(UserRouter.getUserInfo)
+            .validate(statusCode: 200...500)
+            .responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let data = response.data else { return }
+                let networkResult = self.judgeStatus(by: statusCode, data, UserResponse.self)
+
+                completion(networkResult)
+
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
+    func getTwitList(completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        AFmanager.request(UserRouter.getTwitList)
+            .validate(statusCode: 200...500)
+            .responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let data = response.data else { return }
+                let networkResult = self.judgeStatus(by: statusCode, data, Twit.self)
+
+                completion(networkResult)
+
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
+    func likeTwit(postId: Int, completion: @escaping (NetworkResult<Any>) -> (Void)) {
+        AFmanager.request(UserRouter.likeTwit(postId: postId))
+            .validate(statusCode: 200...500)
+            .responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let data = response.data else { return }
+                let networkResult = self.judgeStatus(by: statusCode, data, Like.self)
+
+                completion(networkResult)
+
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+}
